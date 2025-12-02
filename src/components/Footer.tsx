c8af6083f147bd5851fa4ea6b086b84c8cb5eb24
Rhys Dhustin Ocana <ro28@hawaii.edu>
2025-12-01 18:53:35 -1000

    Fixing login dropdown style and added style to Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,35 @@
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-light">
+  <footer className="mt-auto py-3">
     <Container>
-      <Col className="text-center">
-        Department of Information and Computer Sciences
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        <br />
-        <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
-      </Col>
+      <div className="text-center">
+        <h2>Locations</h2>
+        <h3>Retrieval Offices</h3>
+        <Row>
+          <Col>
+            <h4>Office</h4>
+            <p>ICS Admin Office</p>
+            <br />
+            <p>QLCSS</p>
+          </Col>
+          <Col>
+            <h4>Hours</h4>
+            <p>8:00 AM - 4:00 PM</p>
+            <p>8:00 AM - 4:00 PM</p>
+          </Col>
+          <Col>
+            <h4>Days</h4>
+            <p>Monday - Friday</p>
+            <br />
+            <p>Monday - Friday</p>
+          </Col>
+        </Row>
+        <h3>Contact</h3>
+        <p>Phone: (555) 555-5555</p>
+        <p>Email: john@foo.com</p>
+      </div>
     </Container>
   </footer>
 );
